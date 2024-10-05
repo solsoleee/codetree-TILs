99075acfa3d193fc6e41ce8983573717282e6fad
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-10-05 20:21:58 +0900

    Title: 행복한 수열의 개수, Time: 82ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241005/행복한 수열의 개수/number-of-happy-sequence.java
+++ 241005/행복한 수열의 개수/number-of-happy-sequence.java
@@ -29,16 +29,21 @@ public class Main {
         int cnt;
         int res=0;
         for(int i=0; i<n; i++) {
-            cnt = 0;
+            cnt = 1;
             pre = arr[0][i];
-            for(int j=0; j<n; j++) {
+            for(int j=1; j<n; j++) {
                 if(arr[j][i] == pre) {
                     cnt ++;
                 }
-                
+                else{
+                    cnt = 1;
+                }
+                if(cnt >=m) {
+                    res++;
+                    break;
+                }   
                 pre = arr[j][i];
             }
-            if(cnt >= m) res++;
         }
         return res;
     }
@@ -47,16 +52,22 @@ public class Main {
         int cnt;
         int res=0;
         for(int i=0; i<n; i++) {
-            cnt = 0;
+            cnt = 1;
             pre = arr[i][0];
-            for(int j=0; j<n; j++) {
+            for(int j=1; j<n; j++) {
                 if(arr[i][j] == pre) {
                     cnt ++;
                     //System.out.println(pre +" " + arr[i][j]);
                 }
+                else{
+                    cnt = 1;
+                }
+                if(cnt >=m) {
+                    res++;
+                    break;
+                }
                 pre = arr[i][j];
             }
-            if(cnt >= m) res++;
         }
         return res;
     }

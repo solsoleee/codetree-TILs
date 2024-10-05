776f9530f7c7520c4627c8fc38830e5a996b06b8
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-10-05 20:00:24 +0900

    Title: 행복한 수열의 개수, Time: 111ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241005/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static StringTokenizer tokens;
+    static int n,m;
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        tokens = new StringTokenizer(input.readLine());
+        n = Integer.parseInt(tokens.nextToken());
+        m = Integer.parseInt(tokens.nextToken());
+        int array[][] = new int [n][n];
+        for(int i=0; i<n; i++) {
+            tokens = new StringTokenizer(input.readLine());
+            for(int j=0; j<n; j++) {
+                array[i][j] =Integer.parseInt(tokens.nextToken());
+            }
+        }
+        int row = rowCheck(array);
+        int col = colCheck(array);
+
+        System.out.println(row+col);
+    }
+
+    public static int rowCheck(int arr[][]){
+        int pre;
+        int cnt;
+        int res=0;
+        for(int i=0; i<n; i++) {
+            cnt = 1;
+            pre = -1;
+            for(int j=0; j<n; j++) {
+                if(arr[j][i] == pre) {
+                    cnt ++;
+                }
+                
+                pre = arr[j][i];
+            }
+            if(cnt >= m) res++;
+        }
+        return res;
+    }
+        public static int colCheck(int arr[][]){
+        int pre;
+        int cnt;
+        int res=0;
+        for(int i=0; i<n; i++) {
+            cnt = 1;
+            pre = -1;
+            for(int j=0; j<n; j++) {
+                if(arr[i][j] == pre) {
+                    cnt ++;
+                    //System.out.println(pre +" " + arr[i][j]);
+                }
+                pre = arr[i][j];
+            }
+            if(cnt >= m) res++;
+        }
+        return res;
+    }
+}

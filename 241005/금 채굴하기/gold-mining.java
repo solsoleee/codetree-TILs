fea25e45947ddd16cb5a9ed15584fcf5d8c82e89
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-10-05 21:39:09 +0900

    Title: 금 채굴하기, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241005/금 채굴하기/gold-mining.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringTokenizer tokens;
+    static StringBuilder output = new StringBuilder();
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static int n,m;
+    static int map[][];
+    public static void main(String[] args) throws IOException {
+        tokens = new StringTokenizer(input.readLine());
+        n = Integer.parseInt(tokens.nextToken());
+        m =Integer.parseInt(tokens.nextToken());
+        map = new int [n][n];
+        for(int i=0; i<n; i++) {
+            tokens = new StringTokenizer(input.readLine());
+            for(int j=0; j<n; j++) {
+                map[i][j] = Integer.parseInt(tokens.nextToken());
+            }
+        }
+        int r = n/2;
+        int count;
+        int maxVal = Integer.MIN_VALUE;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+
+                count=0;
+                for(int row = 0; row<n; row++) {
+                    for(int col=0; col<n; col++ ) {
+                        if( Math.abs(i-row) + Math.abs(j-col) <= r){
+                            if(map[row][col] == 1) count++;
+                        }
+                    }
+                }
+                maxVal = Math.max(maxVal, count);
+            }
+        }
+        System.out.print(maxVal);
+    }
+
+}

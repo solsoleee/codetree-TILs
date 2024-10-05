f3b64b7a553e5572716da39be955b7cf1cf90518
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-10-05 20:22:58 +0900

    Title: 행복한 수열의 개수, Time: 103ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241005/행복한 수열의 개수/number-of-happy-sequence.java
+++ 241005/행복한 수열의 개수/number-of-happy-sequence.java
@@ -4,69 +4,69 @@ import java.util.*;
 public class Main {
 
     static StringTokenizer tokens;
-    static int n,m;
+    static int n, m;
     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-    public static void main(String[] args) throws IOException{
-        // 여기에 코드를 작성해주세요.
+
+    public static void main(String[] args) throws IOException {
         tokens = new StringTokenizer(input.readLine());
         n = Integer.parseInt(tokens.nextToken());
         m = Integer.parseInt(tokens.nextToken());
-        int array[][] = new int [n][n];
-        for(int i=0; i<n; i++) {
+        int array[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
             tokens = new StringTokenizer(input.readLine());
-            for(int j=0; j<n; j++) {
-                array[i][j] =Integer.parseInt(tokens.nextToken());
+            for (int j = 0; j < n; j++) {
+                array[i][j] = Integer.parseInt(tokens.nextToken());
             }
         }
         int row = rowCheck(array);
         int col = colCheck(array);
 
-        System.out.println(row+col);
+        System.out.println(row + col);
     }
 
-    public static int rowCheck(int arr[][]){
+    // 행을 체크하는 함수
+    public static int rowCheck(int arr[][]) {
         int pre;
         int cnt;
-        int res=0;
-        for(int i=0; i<n; i++) {
+        int res = 0;
+        for (int i = 0; i < n; i++) {
             cnt = 1;
-            pre = arr[0][i];
-            for(int j=1; j<n; j++) {
-                if(arr[j][i] == pre) {
-                    cnt ++;
-                }
-                else{
+            pre = arr[i][0]; // 행을 기준으로
+            for (int j = 1; j < n; j++) {
+                if (arr[i][j] == pre) {
+                    cnt++;
+                } else {
                     cnt = 1;
                 }
-                if(cnt >=m) {
-                    res++;
-                    break;
-                }   
-                pre = arr[j][i];
-            }
-        }
-        return res;
-    }
-        public static int colCheck(int arr[][]){
-        int pre;
-        int cnt;
-        int res=0;
-        for(int i=0; i<n; i++) {
-            cnt = 1;
-            pre = arr[i][0];
-            for(int j=1; j<n; j++) {
-                if(arr[i][j] == pre) {
-                    cnt ++;
-                    //System.out.println(pre +" " + arr[i][j]);
-                }
-                else{
-                    cnt = 1;
-                }
-                if(cnt >=m) {
+                if (cnt >= m) {
                     res++;
                     break;
                 }
                 pre = arr[i][j];
+            }
+        }
+        return res;
+    }
+
+    // 열을 체크하는 함수
+    public static int colCheck(int arr[][]) {
+        int pre;
+        int cnt;
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            cnt = 1;
+            pre = arr[0][i]; // 열을 기준으로
+            for (int j = 1; j < n; j++) {
+                if (arr[j][i] == pre) {
+                    cnt++;
+                } else {
+                    cnt = 1;
+                }
+                if (cnt >= m) {
+                    res++;
+                    break;
+                }
+                pre = arr[j][i];
             }
         }
         return res;

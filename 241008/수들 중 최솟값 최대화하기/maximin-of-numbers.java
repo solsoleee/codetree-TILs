7ab6f1db3c65b2874066a24197c275b6ae8dd76f
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-10-08 15:49:14 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 146ms, Memory: 9MB, Status: Passed - CodeTree

--- 241008/수들 중 최솟값 최대화하기/maximin-of-numbers.java
+++ 241008/수들 중 최솟값 최대화하기/maximin-of-numbers.java
@@ -4,53 +4,45 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    
+    static StringTokenizer tokens;
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static int n;
+    static int arr[][];
+    static boolean visitedRow[];
+    static boolean visitedCol[];
+    static int maxVal = Integer.MIN_VALUE;
 
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(input.readLine());
+        arr = new int[n][n];
+        visitedRow = new boolean[n];
+        visitedCol = new boolean[n];
 
-	static StringTokenizer tokens;
-	static StringBuilder output = new StringBuilder();
-	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	static int n;
-	static int arr[][];
-	static boolean visitedRow[];
-	static boolean visitedCol[];
-	static int res[];
-	static int maxVal = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            tokens = new StringTokenizer(input.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(tokens.nextToken());
+            }
+        }
+        dfs(0, Integer.MAX_VALUE);
+        System.out.println(maxVal);
+    }
 
-	public static void main(String[] args) throws IOException {
-		n = Integer.parseInt(input.readLine());
-		arr =new int [n][n];
-		visitedRow = new boolean[n];
-		visitedCol = new boolean[n];
-		res = new int[n];
+    static void dfs(int cnt, int currentMin) {
+        if (cnt == n) {
+            maxVal = Math.max(maxVal, currentMin);
+            return;
+        }
 
-		for(int i=0; i<n; i++) {
-			tokens = new StringTokenizer(input.readLine());
-			for(int j=0; j<n; j++) {
-				arr[i][j] = Integer.parseInt(tokens.nextToken());
-			}
-		}
-		permutation(0, Integer.MAX_VALUE);
-		System.out.println(maxVal);
-	}
-	static void permutation(int cnt, int currentMin) {
-		if(cnt == n) {
-			// for(int i=0; i<n; i++) {
-			// 	min = Math.min(min, res[i]);
-			// }
-			maxVal = Math.max(maxVal, currentMin);
-			return;
-		}
-		for(int i=0; i<n*n; i++) {
-			int row = i/n;
-			int col = i%n;
-			if(!visitedRow[row] && !visitedCol[col]) {
-				res[cnt] = arr[row][col];
-				visitedRow[row] = true;
-				visitedCol[col] = true;
-				permutation(cnt+1, Math.min(currentMin, arr[row][col]));
-				visitedRow[row] = false;
-				visitedCol[col] = false;
-			}
-		}
-	}
+        for (int i = 0; i < n; i++) {
+            if (!visitedRow[cnt] && !visitedCol[i]) {
+                visitedRow[cnt] = true;
+                visitedCol[i] = true;
+                dfs(cnt + 1, Math.min(currentMin, arr[cnt][i]));
+                visitedRow[cnt] = false;
+                visitedCol[i] = false;
+            }
+        }
+    }
 }

3c839ddbc1fe1a291e3356f82bcbb6630e6425ce
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-10-09 00:38:18 +0900

    Title: 최단 Run Length 인코딩, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

--- 241009/최단 Run Length 인코딩/shortest-run-length-encoding.java
+++ 241009/최단 Run Length 인코딩/shortest-run-length-encoding.java
@@ -11,39 +11,17 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        String str[] = input.readLine().split("");
-        int len = str.length;
-        String temp[] = new String[len];
-
-        //System.out.println(len);
-        //길이 완전 탐색
-        for(int i=0; i<len; i++) {
-            //배열 바뀜
-            for(int a=0; a< len-i; a++) {
-                temp[a+i] = str[a];
-            }
-
-            String temp2[] = new String[i];
-            for(int b=0; b<i; b++) {
-                temp2[b] = str[len-i+b];
-            }for(int c=0; c<i; c++) {
-                String t = temp2[c];
-                temp2[c] = temp2[i-c-1];
-                temp2[i-c-1] = t;
-            }
-            for(int j=0; j<i; j++) {
-                temp[j] = temp2[j];
-            }
-
-            //최종으로 만들어짐
-            //System.out.println(Arrays.toString(temp));
-
+        String str = input.readLine();
+        String newS = "";
+        int n = str.length();
+        for(int i=0; i<n; i++) {
+            newS = str.substring(n-1-i) + str.substring(0,n-1-i);
             //문자열 합축하기
-            String pre = temp[0];
+            char pre = newS.charAt(0);
             int cnt=1;
             String newStr="";
-            for(int s=1; s<len; s++) {
-                if(pre.equals(temp[s])) {
+            for(int s=1; s<n; s++) {
+                if(pre == newS.charAt(s)) {
                     cnt+=1;
                 }
                 else{
@@ -52,12 +30,12 @@ public class Main {
                     newStr += String.valueOf(cnt);
                     cnt=1;
                 }
-                pre = temp[s];
+                pre = newS.charAt(s);
             }
             newStr += (pre + String.valueOf(cnt));
             minVal = Math.min(newStr.length(), minVal);
 
         }
         System.out.print(minVal);
-    }
+        }
 }

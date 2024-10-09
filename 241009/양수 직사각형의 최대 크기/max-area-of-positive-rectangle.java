3107534bd7adc9d294fb0a702e46901999cdf572
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-10-09 14:38:29 +0900

    Title: 양수 직사각형의 최대 크기, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241009/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main{
+    static StringTokenizer tokens;
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static int n,m;
+    static int maxVal = Integer.MIN_VALUE;
+    static int map[][];
+    static int arr[] = new int [4];
+
+    public static void main(String[] args) throws IOException {
+        tokens = new StringTokenizer(input.readLine());
+        n = Integer.parseInt(tokens.nextToken());
+        m = Integer.parseInt(tokens.nextToken());
+        map = new int [n][m];
+        for(int i =0 ; i<n; i++) {
+            tokens = new StringTokenizer(input.readLine());
+            for(int j=0; j<m; j++) {
+                map[i][j] = Integer.parseInt(tokens.nextToken());
+            }
+        }
+
+        rectangle();
+        System.out.println(maxVal);
+
+    }
+    static void rectangle() {
+        //왼쪽 끝점
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+
+                //오른쪽 끝점
+                for(int a=0; a<n; a++) {
+                    for(int b=1; b<m; b++) {
+                        arr[0] = i;
+                        arr[1] =j;
+                        arr[2] = a;
+                        arr[3] = b;
+                        boolean flag = true;
+                        for(int r1=i; r1<=a; r1++) {
+                            if(!flag) break;
+                            for(int c1=j; c1<=b; c1++) {
+                                if (map[r1][c1] < 0)  {
+                                    flag = false;
+                                    break;
+                                }
+                            }
+                        }
+                        int val=0;
+                        if(flag) {
+                            val= (a-i+1)*(b-j+1);
+                        }
+                        maxVal = Math.max(maxVal, val);
+                    }
+                }
+            }
+        }
+    }
+
+}

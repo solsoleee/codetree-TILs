c18a89eaee6078b51af81d0fdec6d857e2dc60bb
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-10-09 14:53:11 +0900

    Title: 양수 직사각형의 최대 크기, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241009/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
+++ 241009/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -1,61 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static StringTokenizer tokens;
     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-    static int n,m;
-    static int maxVal = Integer.MIN_VALUE;
+    static int n, m;
+    static int maxVal = 0;  // 초기값을 0으로 설정
     static int map[][];
 
     public static void main(String[] args) throws IOException {
         tokens = new StringTokenizer(input.readLine());
         n = Integer.parseInt(tokens.nextToken());
         m = Integer.parseInt(tokens.nextToken());
-        map = new int [n][m];
-        for(int i =0 ; i<n; i++) {
+        map = new int[n][m];
+        
+        // 배열 입력 받기
+        for (int i = 0; i < n; i++) {
             tokens = new StringTokenizer(input.readLine());
-            for(int j=0; j<m; j++) {
+            for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(tokens.nextToken());
             }
         }
 
-        rectangle();
-        if(maxVal ==0 ) System.out.println(-1);
-        else System.out.println(maxVal);
-
+        // 최대 크기 양수 직사각형 탐색
+        findMaxRectangle();
+        
+        // 양수 직사각형이 없을 경우 -1 출력
+        if (maxVal == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(maxVal);
+        }
     }
-    static void rectangle() {
-        //왼쪽 끝점
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
 
-                //오른쪽 끝점
-                for(int a=i; a<n; a++) {
-                    for(int b=j; b<m; b++) {
+    static void findMaxRectangle() {
+        // 왼쪽 상단 좌표 (i, j)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
 
-                        boolean flag = true;
-                        for(int r1=i; r1<=a; r1++) {
-                            if(!flag) break;
-                            for(int c1=j; c1<=b; c1++) {
-                                if (map[r1][c1] < 0)  {
-                                    flag = false;
+                // 오른쪽 하단 좌표 (a, b)
+                for (int a = i; a < n; a++) {
+                    for (int b = j; b < m; b++) {
+
+                        // 직사각형 내부가 모두 양수인지 확인
+                        boolean allPositive = true;
+                        for (int r = i; r <= a; r++) {
+                            if (!allPositive) break; // 내부 확인 도중 음수를 발견하면 중단
+                            for (int c = j; c <= b; c++) {
+                                if (map[r][c] < 0) {
+                                    allPositive = false;
                                     break;
                                 }
                             }
                         }
-                        int val=0;
-                        if(flag) {
-                            val= (a-i+1)*(b-j+1);
+
+                        // 양수로만 이루어진 직사각형이면 크기 계산
+                        if (allPositive) {
+                            int area = (a - i + 1) * (b - j + 1);
+                            maxVal = Math.max(maxVal, area);
                         }
-                        maxVal = Math.max(maxVal, val);
                     }
                 }
             }
         }
     }
-
 }

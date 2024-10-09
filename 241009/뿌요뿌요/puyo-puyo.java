ce413b6a801b9ace2008d8744c046d06e8a7b2df
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-10-09 22:27:41 +0900

    Title: 뿌요뿌요, Time: 80ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241009/뿌요뿌요/puyo-puyo.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer tokens;
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static int n,res,sum;
+    static int maxVal = Integer.MIN_VALUE;
+    static int map[][];
+    static boolean visited[][];
+    static int deltas[][] ={{0,1}, {1,0}, {-1,0}, {0,-1}};
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(input.readLine());
+        map = new int [n][n];
+        visited = new boolean[n][n];
+
+        for(int i=0; i<n; i++) {
+            tokens = new StringTokenizer(input.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(tokens.nextToken());
+            }
+        }
+        // 한 점씩 돌아가면서 찾기
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(!visited[i][j]) {
+                    sum=1;
+                    dfs(i,j,1);
+                    if(sum >=4) res++;
+                }
+                maxVal = Math.max(sum, maxVal);
+            }
+        }
+        System.out.println(res +" " +maxVal);
+
+    }
+    static void dfs(int x, int y, int cnt) {
+        if(cnt == n*n) { //다 방문 했으면
+            return;
+        }
+        visited[x][y] =true;
+        for(int d[]: deltas) {
+            int nx = x+d[0];
+            int ny = y+d[1];
+            if(check(nx,ny) && !visited[nx][ny]) { //같은 숫자로 이루어져야함
+                if(map[x][y] == map[nx][ny]) {
+                    sum++;
+                    dfs(nx,ny,cnt+1);
+                }
+            }
+        }
+    }
+    static boolean check(int x, int y) {
+        return x>=0 && x<n && y>=0 && y<n;
+    }
+}

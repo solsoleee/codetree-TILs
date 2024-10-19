879622ed68e75160ce73f1bd59410be4b19a8a7e
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-10-19 15:22:19 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 3015ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

--- 241019/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.java
+++ 241019/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.java
@@ -4,56 +4,53 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static StringTokenizer tokens;
-    static boolean flag;
+    static boolean flag;   // 답을 찾았을 때 탐색 종료를 위한 플래그
     static int n;
-    static int res[];
-    static int arr[] ={4,5,6};
+    static int res[];      // 결과 수열을 저장할 배열
+    static int arr[] = {4, 5, 6};   // 사용할 숫자 배열
 
     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-        n = Integer.parseInt(input.readLine());
-        res = new int[n];
-        permu(0);
+        n = Integer.parseInt(input.readLine());  // n값 입력 받음
+        res = new int[n];  // 결과 수열 크기 n만큼 초기화
+        permu(0);  // 중복 순열 생성 시작
     }
 
-    //중복 순열 만들기
+    // 중복 순열 만들기
     static void permu(int cnt) {
-        if (cnt == n) {
-            if (check()) {
-                // 정답을 찾았으면 출력
-                for (int r : res) {
+        if (cnt == n) {  // 수열의 길이가 n일 때
+            if (check()) {  // 중복 부분 수열이 없는지 확인
+                for (int r : res)  // 정답 출력
                     System.out.print(r);
-                }
-                flag = true;  // 출력 후 멈추기
+                flag = true;  // 정답을 찾았으므로 탐색 종료
             }
             return;
         }
-        
         for (int i = 0; i < 3; i++) {
-            if (flag) break;  // 정답을 찾았으면 더 이상 수행하지 않음
-            res[cnt] = arr[i];
-            permu(cnt + 1);
+            if (flag) break;  // 정답을 찾았으면 더 이상 탐색하지 않음
+            res[cnt] = arr[i];  // 수열에 숫자 추가
+            permu(cnt + 1);  // 다음 자리 재귀 호출
         }
     }
 
+    // 중복된 연속 부분 수열이 있는지 확인하는 함수
     static boolean check() {
-        // 길이 n에 대해 가능한 모든 연속 부분 수열이 같은지 체크
-        for (int len = 1; len <= n / 2; len++) {
-            for (int i = 0; i + len * 2 <= n; i++) {
+        // 연속 부분 수열을 확인하는 과정
+        for (int len = 1; len <= n / 2; len++) {  // 가능한 부분 수열의 길이 범위 탐색
+            for (int i = 0; i + len * 2 <= n; i++) {  // 인접한 두 수열 비교
                 boolean isSame = true;
                 for (int j = 0; j < len; j++) {
-                    if (res[i + j] != res[i + j + len]) {
+                    if (res[i + j] != res[i + j + len]) {  // 수열이 다르면 중복 아님
                         isSame = false;
                         break;
                     }
                 }
-                if (isSame) {
-                    return false; // 중복된 부분 수열 발견
+                if (isSame) {  // 만약 중복된 수열이 발견되면
+                    return false;  // 중복이므로 false 반환
                 }
             }
         }
-        return true; // 중복된 부분 수열이 없으면 true
+        return true;  // 중복이 없으면 true 반환
     }
 }

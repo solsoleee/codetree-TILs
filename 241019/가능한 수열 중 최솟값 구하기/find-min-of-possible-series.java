ae80eec91fde52d644f41ecc33b1574a3ce76ed3
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-10-19 15:37:08 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 3009ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

--- 241019/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.java
+++ 241019/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
     public static int n;
     public static int[] res;     // 수열을 저장할 배열

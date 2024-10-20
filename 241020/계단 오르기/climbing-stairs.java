d7b3667ea08ea963c014900154b069e93fb19deb
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-10-20 14:58:09 +0900

    Title: 계단 오르기, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241020/계단 오르기/climbing-stairs.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringTokenizer tokens;
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(input.readLine());
+        int dp[] = new int[1001];
+        dp[0] = 0;
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
+        for(int i=4; i<=n; i++) {
+            dp[i] = dp[i-2] + dp[i-3];
+        }
+        System.out.println(dp[n]);
+    }
+}

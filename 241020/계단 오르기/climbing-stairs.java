6b8e58fd1b6e732f1d36e8ffa200798babf09424
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-10-20 14:59:31 +0900

    Title: 계단 오르기, Time: 105ms, Memory: 8MB, Status: Passed - CodeTree

--- 241020/계단 오르기/climbing-stairs.java
+++ 241020/계단 오르기/climbing-stairs.java
@@ -14,8 +14,8 @@ public class Main {
         dp[2] = 1;
         dp[3] = 1;
         for(int i=4; i<=n; i++) {
-            dp[i] = dp[i-2] + dp[i-3];
+            dp[i] = ( dp[i-2] + dp[i-3] ) % 10007;
         }
-        System.out.println(dp[n]);
+        System.out.print(dp[n]% 10007);
     }
 }

e6c82185d0e2438b41f0d9894f7d503a8efb8e69
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-10-22 16:04:57 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 108ms, Memory: 9MB, Status: Passed - CodeTree

--- 241022/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.java
+++ 241022/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.java
@@ -7,7 +7,7 @@ public class Main {
     static int n,t,m;
     static int[][] map;
     static int[][] deltas= { {-1,0}, {1,0},{0,-1},{0,1} };
-    static int [][] bead;
+    static int [][] bead, newBead;
 
     public static void main(String[] args) throws IOException {
         tokens = new StringTokenizer(input.readLine());
@@ -15,7 +15,8 @@ public class Main {
         m = Integer.parseInt(tokens.nextToken());
         t = Integer.parseInt(tokens.nextToken());
         map = new int[n][n];
-        bead = new int[m][3];
+        bead = new int[n][n];
+
 
         for(int i=0; i<n; i++) {
             tokens = new StringTokenizer(input.readLine());
@@ -26,53 +27,76 @@ public class Main {
 
         for(int i=0; i<m; i++) {
             tokens = new StringTokenizer(input.readLine());
-            bead[i][0] = Integer.parseInt(tokens.nextToken())-1;
-            bead[i][1] = Integer.parseInt(tokens.nextToken())-1;
-            bead[i][2] = 0; //기본은 0
+            int a = Integer.parseInt(tokens.nextToken())-1;
+            int b = Integer.parseInt(tokens.nextToken())-1;
+            bead[a][b] =1;
         }
+
+
         while(t-- >0) {
+            //움직이고 -> 제거하고 ->  다시 bead업데이트
+            newBead = new int [n][n];
             move();
-            //System.out.println(Arrays.deepToString(bead));
-
+            remove();
         }
-        System.out.println(m);
+        System.out.println(count());
+    }
+
+    static int count() {
+        int sum=0;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(bead[i][j] == 1) {
+                    sum ++;
+                }
+            }
+        }
+        return sum;
+    }
+
+    //2 이상인거 제거하기
+    static void remove() {
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(newBead[i][j] > 1) { //자리
+                    newBead[i][j] =0;
+                }
+            }
+        }
+        bead = newBead;
+    }
 
 
+    //움직이기
+    static void move() {
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(bead[i][j] ==1) { //자리
+                    int r[] = find(i,j);
+                    int x = r[0];
+                    int y = r[1];
+                    newBead[x][y] +=1;
+                }
+            }
+        }
 
     }
 
-    //상하좌우 큰거중에 움직이는거
-    static void move() {
-        int b=0;
-        for(int be[]: bead) {
-            if(be[2] == 1) continue;
-            int max = 0; // 상하좌우 중에 큰거
-            int i = 0; //델타인덱스
-            int index=0; //결정
-            for(int d[]: deltas) {
-                int nx = be[0] + d[0];
-                int ny = be[1] + d[1];
-                if(check(nx,ny) && max < map[nx][ny]) {
-                    //상하좌우 중에 가장 큰거 체크
-                    index = i;
-                    max = map[nx][ny];
-                }
-                i++;
+
+    //가장 큰 값 찾기 상하좌우 중
+    static int[] find(int x, int y) {
+        int max = 0;
+        int res[] = new int[2];
+        for(int d[]: deltas) {
+            int nx = x+d[0];
+            int ny = y+d[1];
+            if( check(nx,ny)  && max < map[nx][ny]) {
+                res[0] = nx;
+                res[1] = ny;
+                max = map[nx][ny];
             }
-            //여기서 겹치는지 아닌지 확인
-            be[0] = be[0] + deltas[index][0];
-            be[1] = be[1] + deltas[index][1];
-            for(int a=0; a<m; a++) {
-                if(a == b) continue;
-                if(bead[a][2] == 1 ) continue;
-                if(bead[a][0] == be[0] && bead[a][1] == be[1]) {
-                    m-=2;
-                    bead[a][2]=1;
-                    be[2]=1;
-                }
-            }
-            b++;
         }
+        return res;
     }
 
 

2a2d34256ea5b3452e26d77956483281ef551bb8
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-10-23 16:39:06 +0900

    Title: 사각형 채우기, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241023/사각형 채우기/rectangle-fill.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringTokenizer tokens;
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder output = new StringBuilder();
+    static int n,res;
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(input.readLine());
+        int dp[] = new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        System.out.print(dp[n]);
+    }
+}

88bfaa113ebb14794010cb50afe463d98e02ad91
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2024-10-23 16:33:11 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 112ms, Memory: 9MB, Status: Passed - CodeTree

--- 241023/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
+++ 241023/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
@@ -2,50 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static StringTokenizer tokens;
-    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder output = new StringBuilder();
-    static long n,res;
-    static boolean visited[];
-    public static void main(String[] args) throws IOException {
-        n = Integer.parseInt(input.readLine());
-        visited = new boolean[1000001];
-        //1로 만드는데 최소 연산 횟수
-        bfs();
-        System.out.println(res);
 
-    }
-    static void bfs() {
-        Queue<long []> que = new ArrayDeque<>();
-        que.offer(new long[] {n, 0});
-        visited[(int)n] = true;
-        while(!que.isEmpty()) {
-            long t[] = que.poll();
-            long x = t[0];
-            long cnt = t[1];
-            //System.out.println(x);
-            if(x == 1) {
-                res = cnt;
-                break;
-            }
+        static StringTokenizer tokens;
+        static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        static StringBuilder output = new StringBuilder();
+        static int n,res;
+        public static void main(String[] args) throws IOException {
+            n = Integer.parseInt(input.readLine());
 
-            if(!visited[(int)x+1]) {
-                que.offer(new long[] {x+1, cnt+1});
-                visited[(int)x+1] = true;
-            }
-            if(!visited[(int)x-1]) {
-                que.offer(new long[] {x-1, cnt+1});
-                visited[(int)x-1] = true;
-            }
-            if(!visited[(int)x/2] && x%2==0) {
-                que.offer(new long[] {x/2, cnt+1});
-                visited[(int)x/2] = true;
-            }
-            if(!visited[(int)x/3] && x%3==0) {
-                que.offer(new long[] {x/3, cnt+1});
-                visited[(int)x/3] = true;
-            }
+            //1로 만드는데 최소 연산 횟수
+            bfs();
+            System.out.println(res);
+
         }
+        static void bfs() {
+            Queue<int []> que = new ArrayDeque<>();
+            Set<Integer> visited = new HashSet<>();
+            que.offer(new int[] {n, 0});
+            visited.add(n);
+            while(!que.isEmpty()) {
+                int t[] = que.poll();
+                int x = t[0];
+                int cnt = t[1];
+                //System.out.println(x);
+                if(x == 1){
+                    res = cnt;
+                    break;
+                }
+                if(x%3 == 0 && !visited.contains(x/3)) {
+                    que.offer(new int[] {x/3, cnt+1});
+                    visited.add(x/3);
+                }
+                if(x%2 == 0 && !visited.contains(x/2)) {
+                    que.offer(new int[] {x/2, cnt+1});
+                    visited.add(x/2);
+                }
+                if(!visited.contains(x+1)) {
+                    que.offer(new int[] {x+1, cnt+1});
+                    visited.add(x+1);
+                }
+                if(!visited.contains(x-1)) {
+                    que.offer(new int[] {x-1, cnt+1});
+                    visited.add(x-1);
+                }
+            }
 
+        }
     }
-}

5c177404ed67602a2c448f9f9ac05b270f39cc58
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-04-14 20:18:30 +0900

    Title: 행복한 수열의 개수, Time: 241ms, Memory: 15MB, Status: Passed - Codetree

--- 250414/행복한 수열의 개수/number-of-happy-sequence.java
+++ 250414/행복한 수열의 개수/number-of-happy-sequence.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-  
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -16,36 +16,34 @@ public class Main {
         // 행 탐색
         for (int i = 0; i < n; i++) {
             int count = 1;
-            boolean found = false; // 한 행에서 하나만 세기
+            int maxCount = 1;
             for (int j = 1; j < n; j++) {
                 if (grid[i][j] == grid[i][j-1]) {
                     count++;
                 } else {
                     count = 1;
                 }
-                
-                if (count >= m && !found) {
-                    answer++;
-                    found = true;
-                }
+                maxCount = Math.max(maxCount, count);
+            }
+            if (maxCount >= m) {
+                answer++;
             }
         }
         
         // 열 탐색
         for (int i = 0; i < n; i++) {
             int count = 1;
-            boolean found = false;
+            int maxCount = 1;
             for (int j = 1; j < n; j++) {
                 if (grid[j][i] == grid[j-1][i]) {
                     count++;
                 } else {
                     count = 1;
                 }
-                
-                if (count >= m && !found) {
-                    answer++;
-                    found = true;
-                }
+                maxCount = Math.max(maxCount, count);
+            }
+            if (maxCount >= m) {
+                answer++;
             }
         }
         

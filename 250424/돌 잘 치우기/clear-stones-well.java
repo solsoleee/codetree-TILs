3f3aea12c223be60d95bae31f34e333af5c9d3b4
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-04-24 07:05:21 +0900

    Title: 돌 잘 치우기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250424/돌 잘 치우기/clear-stones-well.java
+++ 250424/돌 잘 치우기/clear-stones-well.java
@@ -2,10 +2,13 @@ import java.util.*;
 public class Main {
     static int n,k,m;
     static int [][] grid;
+    static int [][] res;
     static int [][] startPoints;
     static int [][] copyGrid;
     static int [][] deltas = {{0,1},{1,0},{0,-1},{-1,0}};
     static boolean visited [][];
+    static List<int[]> stone;
+    static int maxVal = Integer.MIN_VALUE;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
@@ -14,40 +17,75 @@ public class Main {
         grid = new int[n][n];
         copyGrid = new int[n][n];
         visited = new boolean[n][n];
+        stone = new ArrayList<>();
+        res = new int[m][2];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 grid[i][j] = sc.nextInt();
+                if(grid[i][j] == 1) {
+                    stone.add(new int[]{i,j});
+                }
             }
         }
+
         
-        //System.out.println(Arrays.deepToString(copyGrid));
+        //System.out.println(Arrays.toString(stone.get(0)));
         startPoints = new int[k][2];
         for (int i = 0; i < k; i++) {
             startPoints[i][0] = sc.nextInt()-1;
             startPoints[i][1] = sc.nextInt()-1;
         }
-        int maxVal = Integer.MIN_VALUE;
-        for(int s[]: startPoints) {
-            int res = bfs(s[0],s[1], m);
-            maxVal = Math.max(maxVal, res);
-        }
-        System.out.println(maxVal);
+
         
         // Please write your code here.
+        permu(0,0);
+        System.out.println(maxVal);
     }
+    //돌 중에서 m개를 조합으로 고름
+    static void permu(int start, int depth) {
+        if(depth == m ) {
+            //여기서 bfs를 고름
+            //System.out.println(Arrays.deepToString(res));
+            //여기서 copyGrid의 위치가 1->0
+            init(); //초기화
+            for(int r[]: res) {
+                copyGrid[r[0]][r[1]] = 0;
+            }
+            //bfs를 돌려서 최대값.
+            
+            for(int s[]: startPoints) {
+                int ans = bfs(s[0],s[1]);
+                maxVal = Math.max(maxVal, ans);
+            }
+            
+
+            return;
+            
+        }
+        for(int i=start; i<stone.size(); i++) {
+            int t[] = stone.get(i);
+            res[depth][0] = t[0];
+            res[depth][1] = t[1];
+            permu(i+1, depth+1);
+        }
+    }
+
+
     //초기화 함수
     static void init() {
         for(int i=0; i<n; i++) {
             copyGrid[i] = Arrays.copyOf(grid[i],n);
-            Arrays.fill(visited[i], false);
         }
     }
+
     static boolean check(int x, int y) {
         return x>=0 && x<n && y>=0 && y<n;
     }
-    static int bfs(int x, int y, int c) {
-        init();
-        int mCount = c;
+
+    static int bfs(int x, int y) {
+        for(int i=0; i<n; i++) {
+            Arrays.fill(visited[i], false);
+        }
         Queue<int []> que = new ArrayDeque<>();
         que.offer(new int [] {x,y});
         visited[x][y] = true;
@@ -66,20 +104,10 @@ public class Main {
                         visited[nx][ny] = true;
                         cnt++;
                     }
-                    else{ //돌일 때, m이 남아 있으면..
-                        if(mCount > 0) {
-                            copyGrid[nx][ny] =0;
-                            mCount--;
-                            que.offer(new int[]{nx,ny});
-                            visited[nx][ny] = true;
-                            cnt++;
-                        }
-                    }
+  
                 }
 
             }
-            
-
         }
         return cnt;
     }

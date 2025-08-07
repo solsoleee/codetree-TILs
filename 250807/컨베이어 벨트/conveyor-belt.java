85734f0b4514cf113f027a76f463012e26d02f32
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-08-07 23:37:08 +0900

    Title: 컨베이어 벨트, Time: 179ms, Memory: 11MB, Status: Passed - Codetree

--- 250807/컨베이어 벨트/conveyor-belt.java
+++ 250807/컨베이어 벨트/conveyor-belt.java
@@ -1,67 +1,64 @@
 import java.util.*;
+
 public class Main {
-    static int n;
-    static int t;
-    static int [] top;
-    static int [] bottom;
+    static int n, t;
+    static int[] top, bottom;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         t = sc.nextInt();
         top = new int[n];
         bottom = new int[n];
+
         for (int i = 0; i < n; i++) {
             top[i] = sc.nextInt();
         }
         for (int i = 0; i < n; i++) {
             bottom[i] = sc.nextInt();
         }
-        while(t>0) {
-            int b[] = new int [n];
-        for(int i=0; i<n; i++) {
-            b[i] = bottom[i];
-        }
-        // Please write your code here.
-        //System.out.println(Arrays.toString(bottom));
-        //change(b);
-        //System.out.println(Arrays.toString(bottom));
-        //첫째줄은 오른쪽으로 가기
-        int topT = top[n-1];
-        for(int i=n-1; i>0; i--) {
-            top[i] = top[i-1];
-        }
-        //좌우반전
-        change(b);
-        // 아래쪽은 왼쪽으로
-        int bottomT = bottom[0];
-        for(int i=0; i<n-1; i++) {
-            bottom[i] = bottom[i+1];
-        }
-        top[0] = bottomT;
-        bottom[n-1] = topT;
-        //다시 좌우반전
-        for(int i=0; i<n; i++) {
-            b[i] = bottom[i];
-        }
-        change(b);
 
-        t--;
+        for (int k = 0; k < t; k++) {
+            // 1. top 한 칸 오른쪽 회전
+            int lastTop = top[n - 1];
+            for (int i = n - 1; i > 0; i--) {
+                top[i] = top[i - 1];
+            }
+
+            // 2. bottom 좌우 반전
+            reverse(bottom);
+
+            // 3. bottom 한 칸 왼쪽 회전
+            int firstBottom = bottom[0];
+            for (int i = 0; i < n - 1; i++) {
+                bottom[i] = bottom[i + 1];
+            }
+
+            // 4. top과 bottom 연결
+            top[0] = firstBottom;
+            bottom[n - 1] = lastTop;
+
+            // 5. bottom 다시 좌우 반전
+            reverse(bottom);
         }
-        //System.out.println(Arrays.toString(top));
-        //System.out.println(Arrays.toString(bottom));
-        for(int i=0; i<n; i++) {
-            System.out.print(top[i] +" ");
-        }
-        System.out.println();
-        for(int i=0; i<n; i++) {
-            System.out.print(bottom[i] +" ");
-        }
-        
+
+        // 출력
+        printArray(top);
+        printArray(bottom);
     }
-    //좌우 반전
-    static void change(int temp[]) {
-        for(int i=0; i<n; i++) {
-            bottom[i] = temp[n-i-1];
+
+    // 배열 좌우 반전
+    static void reverse(int[] arr) {
+        for (int i = 0; i < n / 2; i++) {
+            int tmp = arr[i];
+            arr[i] = arr[n - 1 - i];
+            arr[n - 1 - i] = tmp;
         }
+    }
+
+    // 배열 출력 함수
+    static void printArray(int[] arr) {
+        for (int x : arr) System.out.print(x + " ");
+        System.out.println();
     }
 }

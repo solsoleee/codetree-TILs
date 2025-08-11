c37545770c747c3a725d0683b4294a26a23aad1a
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-08-12 03:13:31 +0900

    Title: 수들의 합 최대화하기, Time: 132ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250812/수들의 합 최대화하기/max-sum-of-numbers.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class Main {
+    static int n;
+    static int[][] grid;
+    static boolean[] visited;
+    static int[] arr;
+    static int[] res;
+    static int max;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        arr = new int[n];
+        grid = new int[n][n];
+        visited = new boolean[n];
+        max = Integer.MIN_VALUE;
+        res = new int[n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+        for(int i=0; i<n; i++) {
+            arr[i] = i;
+        }
+        permu(0);
+        System.out.println(max);
+    }
+    static void permu(int depth) {
+        if(depth == n) {
+            //System.out.println(Arrays.toString(res));
+            int sum =0;
+            for(int i=0; i<n; i++) {
+                
+                int r = res[i];
+                sum += grid[i][r] ;
+            }
+            max = Math.max(max, sum);
+            
+            return;
+        }
+        for(int i=0; i<n; i++) {
+            if(!visited[i]) {
+
+                    res[depth] = arr[i];
+                    visited[i] = true;
+                    permu(depth+1);
+                    visited[i] = false;
+                
+            }
+        }
+    }
+
+}

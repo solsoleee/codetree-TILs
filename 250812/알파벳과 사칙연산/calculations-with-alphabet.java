91235cc58ac964e1f6d3c27c1edf6fca0d3a9dae
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-08-12 22:49:56 +0900

    Title: 알파벳과 사칙연산, Time: 136ms, Memory: 10MB, Status: Passed - Codetree

--- 250812/알파벳과 사칙연산/calculations-with-alphabet.java
+++ 250812/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -1,58 +1,45 @@
 import java.util.*;
-
 public class Main {
+    static int num[];
+    static Set<Integer> list = new HashSet<>();
     static String expression;
-    static int len;                // 알파벳 개수
-    static int[] res;               // 알파벳 자리 숫자 배치
-    static List<Character> ops;     // 연산자 목록
-    static int maxVal = Integer.MIN_VALUE;
-
+    static int maxval = Integer.MIN_VALUE;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         expression = sc.next();
-        sc.close();
-
-        len = 0;
-        ops = new ArrayList<>();
-        // 알파벳 자리 세기 + 연산자 저장
-        for (int i = 0; i < expression.length(); i++) {
-            char ch = expression.charAt(i);
-            if (ch >= 'a' && ch <= 'f') {
-                len++;
-            } else {
-                ops.add(ch);
-            }
-        }
-
-        res = new int[len];
+        // Please write your code here.
+        num = new int[6];
         permu(0);
-        System.out.println(maxVal);
+        System.out.println(maxval);
     }
-
-    // 1~4 중복순열
+    static int cal() {
+        int n = expression.charAt(0) - 'a';
+        int s = num[n];
+        int sum =0;
+        for(int i=1; i<expression.length(); i+=2) {
+            char c = expression.charAt(i);
+            int temp = expression.charAt(i+1) -'a';
+            if(c == '-') {
+                s= s-num[temp];
+            }
+            else if(c=='+') {
+                s = s+num[temp];
+            }
+            else s = s*num[temp];
+            
+        }
+        return s;
+    }
     static void permu(int depth) {
-        if (depth == len) {
-            int val = cal();
-            maxVal = Math.max(maxVal, val);
+        if(depth == 6) {
+            //계산
+            int t = cal();
+            maxval = Math.max(t, maxval);
             return;
         }
-        for (int num = 1; num <= 4; num++) {
-            res[depth] = num;
-            permu(depth + 1);
+        for(int i=0; i<4; i++) {
+            num[depth] = i+1;
+            permu(depth+1);
         }
-    }
-
-    // 수식 계산
-    static int cal() {
-        int idx = 0; // res 인덱스
-        int c = res[idx++]; // 첫 번째 숫자
-        for (int i = 0; i < ops.size(); i++) {
-            char op = ops.get(i);
-            int next = res[idx++];
-            if (op == '+') c = c + next;
-            else if (op == '-') c = c - next;
-            else c = c * next; // '*'
-        }
-        return c;
     }
 }

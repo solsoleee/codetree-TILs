930d180e7639d85c44cc45b4da4ebd7b7a3ced6e
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-10-25 13:18:18 +0900

    Title: 부분 수열의 합이 K, Time: 188ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 251025/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int [] prepixSum = new int[n+1];
+        prepixSum[0] =0;
+        int ans =0;
+        for(int i=0; i<n; i++) {
+            prepixSum[i+1] = prepixSum[i] + arr[i];
+        }
+        
+        for(int i=0; i<=n; i++) {
+            for(int j=i; j<=n; j++) {
+                int t = prepixSum[j]-prepixSum[i];
+                if(t == k) ans++;
+            }
+        }
+        // System.out.println(Arrays.toString(prepixSum));
+        System.out.println(ans);
+    }
+}

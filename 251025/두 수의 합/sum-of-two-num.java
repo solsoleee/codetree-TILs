8a5c11c373d1ea8f99d96191a36e3f4b7243c537
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-10-25 11:23:27 +0900

    Title: 두 수의 합, Time: 782ms, Memory: 35MB, Status: Passed - Codetree

--- 251025/두 수의 합/sum-of-two-num.java
+++ 251025/두 수의 합/sum-of-two-num.java
@@ -5,28 +5,19 @@ public class Main {
         int n = sc.nextInt();
         int k = sc.nextInt();
         int[] arr = new int[n];
-        boolean[] visited = new boolean[n];
-        int answer = 0;
+        int ans = 0;
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
-            map.put(arr[i],i);
         }
-        // Please write your code here.
         for(int i=0; i<n; i++) {
-            int temp = k-arr[i];
-            if(visited[i]) continue;
-            
-            if(map.containsKey(temp)) {
-                    visited[map.get(temp)] = true;
-                    visited[i] = true;
-                    answer++;
+            int diff = k-arr[i];
+            if(map.containsKey(diff)){
+                ans += map.get(diff);
             }
-
-
+            map.put(arr[i], map.getOrDefault(arr[i],0)+1 );
         }
-        System.out.println(answer);
-        
-        //System.out.println(answer/2);
+        //System.out.println(map);
+        System.out.println(ans);
     }
 }

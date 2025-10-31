08127d46cefa82311ce94a022504790919c296b4
Hansol Wang <86814993+solsoleee@users.noreply.github.com>
2025-10-31 10:34:10 +0900

    Title: 가장 짧은 부분합, Time: 166ms, Memory: 13MB, Status: Passed - Codetree

--- 251031/가장 짧은 부분합/shortest-subtotal.java
+++ 251031/가장 짧은 부분합/shortest-subtotal.java
@@ -1,53 +1,33 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int s = sc.nextInt();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
         int[] arr = new int[n];
-        int tempsum = 0;
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-            tempsum += arr[i];
-        }
-        boolean flag = true;
-        //전체를 다 더했는데 s가 안 넘으면 -1
-        if(tempsum < s)  flag = false;
-        // i, j
-        int ans = Integer.MAX_VALUE; //구간
-        if(flag) {
-            
-            
-            for(int i=0; i<n; i++) {
-                int sum = 0;
-                int j = i;
-                int diff=0;
-                while(j<n && sum<=s ) {
-                    sum += arr[j];
-                    if(sum >= s) {
-                        diff = j-i;
-                        break;
-                    }
-                    j++;
-                }            
-                //구간
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
 
-                if(sum >= s) {
-                        //System.out.println(i +" " + j +" " + diff);
-                        ans = Math.min(diff+1, ans);
-                        
-                }
-                
-                
+        int ans = Integer.MAX_VALUE;
+        int sum = 0, r = 0;
 
+        for (int l = 0; l < n; l++) {
+            // r를 가능한 데까지 늘려 합을 s 이상으로 만든다
+            while (r < n && sum < s) {
+                sum += arr[r++];
             }
+            // 현재 l에서의 최소 길이 갱신
+            if (sum >= s) ans = Math.min(ans, r - l);
+
+            // l을 한 칸 밀면서 윈도우 축소
+            sum -= arr[l];
         }
-        if(flag) {
-            System.out.println(ans);
-        }
-        else {
-            System.out.println(-1);
-        }
-        
+
+        System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
+        // ※ 어떤 문제는 불가능하면 0을 출력하게 되어있음(BOJ 1806). 요구사항에 맞춰 -1/0 선택.
     }
 }
